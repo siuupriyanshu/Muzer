56d257992de61aa1a077241029ceba4f23973a19
siuupriyanshu <mandalpriyanshu22@gmail.com>
2024-11-15 20:44:12 +0545

    landing page

--- next-app/app/components/Appbar.tsx
+++ next-app/app/components/Appbar.tsx
@@ -1,17 +1,26 @@
 "use client"
 
 import { signIn, signOut, useSession } from "next-auth/react"
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const Appbar = () => {
     const session = useSession();
+    const router = useRouter();
   return (
-    <div className="flex justify-between">
-   <div>
-      Muzi
-    </div>
-    <div>
-        {session.data?.user && <button className="m-2 p-2 bg-fuchsia-600" onClick={() => signOut()}>Logout</button> }
-       {!session.data?.user &&  <button className="m-2 p-2 bg-fuchsia-600" onClick={() => signIn()}>Sign In</button>}
+    <div  className="flex justify-between px-5 py-4 md:px-10 xl:px-20">
+    <div
+        onClick={() => {
+          router.push("/home");
+        }}
+        className={`flex flex-col justify-center text-lg font-bold hover:cursor-pointer `}
+      >
+        Muzer
+      </div>
+    <div className="flex items-center gap-x-2">
+        {session.data?.user && <Button  className="bg-purple-600 text-white hover:bg-purple-700" onClick={() => signOut()}>Logout</Button> }
+       {!session.data?.user &&  <Button  className="bg-purple-600 text-white hover:bg-purple-700" onClick={() => signIn()}>Sign In</Button>}
     </div>
     </div>
     
